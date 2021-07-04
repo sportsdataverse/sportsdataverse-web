724d8c1b63a2f27b41441467b785218310bbfd34
Saiem Gilani <saiem.gilani@gmail.com>
2021-07-04 08:21:02 -0400

    image optimization

--- pages-sections/LandingPage-Sections/PythonPackageSection.js
+++ pages-sections/LandingPage-Sections/PythonPackageSection.js
@@ -87,7 +87,7 @@ export default function PythonPackageSection() {
           </GridItem>
           <GridItem xs={12} sm={12} md={4}>
             <Card plain>
-              <GridItem xs={12} sm={12} md={8}>
+              <GridItem xs={12} sm={12} md={8} className={classes.itemGrid}>
               <Link href='https://wehoop-py.sportsdataverse.org/'>
                 <Image
                   src={wehoopPy}
